25e3eb65b1c9b3a79901247e0ebbcef064d777ca
AlexPetrov <aleksandar2001_1995@abv.bg>
2024-02-13 00:40:03 +0200

    Add redis integration and save chats in redis and db

---
+++ src/config/db/migrations/20240212194719_create-chat-details-table.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('chat_details', function(table){
+    table.increments('id').primary();
+    table.string('from', 70).unique();
+    table.string('to', 70).unique();
+    table.integer('chat_id').unique();
+    table.json("chat")
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('chat_details');
+};
